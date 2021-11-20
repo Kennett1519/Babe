720c48e08876387a9cc93e47fc33fa2efa6cfb59
Kennett Ramirez <73551481+Kennett1519@users.noreply.github.com>
2021-11-20 02:55:14 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function mostrarRespuesta() {
+    if (document.getElementById('respuesta1').checked) {
+        alert(
+            'Felicidades mi so, has tomado la mejor decision en tu vida. Te amo mucho mi niña consentida. (La segunda respuesta no se puede seleccionar pork ajá, sé que un no no iba a ser la respuesta, ups)'
+        );
+    }
+
+    if (document.getElementById('respuesta3').checked) {
+        alert(
+            'Yo sabía que si cherías mi amor, es imposible resistirse ante mi encanto. Te amo mucho mi niña consentida. (La segunda respuesta no se puede seleccionar pork ajá, sé que un no no iba a ser la respuesta, ups)'
+        );
+    }
+}
